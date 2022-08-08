935c9ffd9414d54576d063903a5e390715d14abd
hanafiabdilah <handilah81@gmail.com>
2022-08-08 12:35:14 +0700

    UPDATE: axios interceptors

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -28,11 +28,37 @@ const Dashboard = () => {
     }
   }
 
+  const axiosJWT = axios.create()
+
+  axiosJWT.interceptors.request.use(async (config) => {
+    const currentDate = new Date()
+    if (exp * 1000 < currentDate.getTime()) {
+      const response = await axios.get('http://localhost:5000/token')
+      config.headers.Authorization = `Bearer ${response.data.accessToken}`
+      setToken(response.data.accessToken)
+      const decode = jwt_decode(response.data.accessToken)
+      setName(decode.name)
+      setExp(decode.exp)
+    }
+    return config
+  })
+
+  const getUsers = async () => {
+    const response = await axiosJWT.get('http://localhost:5000/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    console.log(response.data)
+  }
+
   return (
     <>
       <Navbar />
       <div className="container mt-5">
         <h1>Welcome, {name} ! </h1>
+        <button onClick={getUsers}>Get Users</button>
       </div>
     </>
   )
